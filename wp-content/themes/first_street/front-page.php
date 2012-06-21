8894295f9b317809845e74f3d0b4554f93418794
Lawson Kurtz <lawson.kurtz@gmail.com>
2012-06-21 16:19:40 -0400

    grouped all widgets, style changes

--- wp-content/themes/first_street/front-page.php
+++ wp-content/themes/first_street/front-page.php
@@ -1,21 +1,21 @@
 <?php get_header(); ?>
   <ul id="tab-nav" class="nav tabbed-nav inner group">
-    <li class="uppercase white teal-bg">Lobbyists</li>
-    <li class="uppercase white purple-bg">Media</li>
-    <li class="uppercase white orange-bg">Associations</li>
-    <li class="uppercase white green-bg">Government</li>
-    <li class="uppercase white red-bg">Corporations</li>
-    <li class="uppercase white yellow-bg">Education</li>
-    <li class="uppercase white dark-gray-bg">Research</li>
+    <li class="uppercase white teal-bg"><a href="#">Lobbyists</a></li>
+    <li class="uppercase white purple-bg"><a href="#">Media</a></li>
+    <li class="uppercase white orange-bg"><a href="#">Associations</a></li>
+    <li class="uppercase white green-bg"><a href="#">Government</a></li>
+    <li class="uppercase white red-bg"><a href="#">Corporations</a></li>
+    <li class="uppercase white yellow-bg"><a href="#">Education</a></li>
+    <li class="uppercase white dark-gray-bg"><a href="#">Research</a></li>
   </ul>
   <div class="inner group" id="banner">
 
   </div>
   <div class="inner group" id="content">
-    <div id="page-content">
+    <div id="page-content" class="font-size-22">
       <?php get_template_part("loop", "home"); ?>
     </div>
-    <div class="widget-area large" id="widget-area-1">
+    <div class="widget-area large pull-left group" id="widget-area-1">
       <div class="widget">
         <h3 class="dark-gray uppercase main-font">First Street Research Group</h3>
         <div>
@@ -34,20 +34,19 @@
         </div>
       </div>
     </div>
-    <div class="widget-area small" id="widget-area-2">
+    <div class="widget-area small pull-left group" id="widget-area-2">
       <div class="widget">
-		<a href="#" title="#">
-			<img title="Placeholder" alt="Placeholder" src="http://placekitten.com/g/350/225">
-		</a>
-      </div>
-      <div class="widget">
-		<a href="#" title="#">
-			<img title="Placeholder" alt="Placeholder" src="http://placekitten.com/g/350/95">
-		</a>
-        
+    		<a href="#" title="#">
+    			<img title="Placeholder" alt="Placeholder" src="http://placekitten.com/g/350/225">
+    		</a>
+          </div>
+          <div class="widget">
+    		<a href="#" title="#">
+    			<img title="Placeholder" alt="Placeholder" src="http://placekitten.com/g/350/95">
+    		</a>
       </div>
     </div>
-    <div class="widget-area full" id="widget-area-3">
+    <div class="widget-area full group" id="widget-area-3">
       <div class="widget">
         <div>
           <h4 class="main-font">Pioneers in Political Intelligence</h4>
@@ -66,7 +65,7 @@
         </div>
       </div>
     </div>
-    <div class="widget-area full" id="widget-area-4">
+    <div class="widget-area full group" id="widget-area-4">
       <div class="widget large">
       	<!-- Twitter widget goes here. -->
       </div>
