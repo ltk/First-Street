febbab303d50c683ce3c5a0498989a865e01c15b
Tyler Bruffy <tbruffy@thejakegroup.com>
2012-06-25 15:26:03 -0400

    Finished styling for homepage, added plugin for Image Widget

--- wp-content/themes/first_street/front-page.php
+++ wp-content/themes/first_street/front-page.php
@@ -8,10 +8,10 @@
     <li class="uppercase white yellow-bg"><a href="#">Education</a></li>
     <li class="uppercase white dark-gray-bg"><a href="#">Research</a></li>
   </ul>
-  <div class="inner group" id="banner">
+  <div id="banner"  class="inner group feature-panel">
 
   </div>
-  <div class="inner group" id="content">
+  <div id="content" class="inner group">
     <div id="page-content" class="font-size-22">
       <?php get_template_part("loop", "home"); ?>
     </div>
@@ -25,7 +25,7 @@
           <p>
             <span class="published orange strong">May 22, 2012 -</span>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam velit felis, gravida sed bibendum vel, commodo ac elit. Nam quis risus enim, ut convallis diam. Aliquam lacinia lectus ut est laoreet sit amet porttitor metus commodo. Aliquam erat volutpat. Donec nisl nisl, hendrerit a egestas ac, adipiscing dignissim dolor. Nulla nulla tellus, aliquam eu malesuada in, pellentesque tincidunt est.
-            <a class="read-more orange strong">More</a>
+            <a class="read-more orange strong right-arrow">More</a>
           </p>
         </div>
       </div>
@@ -33,19 +33,19 @@
         <h3 class="dark-gray uppercase main-font">What's New at First Street</h3>
         <div>
           <h4 class="dark-gray main-font">First Street Partners with ALL</h4>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam velit felis, gravida sed bibendum vel, commodo ac elit. Nam quis risus enim, ut convallis diam. Aliquam lacinia lectus ut est laoreet sit amet porttitor metus commodo. Aliquam erat volutpat. Donec nisl nisl, hendrerit a egestas ac, adipiscing dignissim dolor. Nulla nulla tellus, aliquam eu malesuada in, pellentesque tincidunt est. <a class="read-more orange strong">More</a></p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam velit felis, gravida sed bibendum vel, commodo ac elit. Nam quis risus enim, ut convallis diam. Aliquam lacinia lectus ut est laoreet sit amet porttitor metus commodo. Aliquam erat volutpat. Donec nisl nisl, hendrerit a egestas ac, adipiscing dignissim dolor. Nulla nulla tellus, aliquam eu malesuada in, pellentesque tincidunt est. <a class="read-more orange strong right-arrow">More</a></p>
         </div>
       </div>
     </div>
     <div class="widget-area small pull-left group" id="widget-area-2">
-      <div class="widget feature-panel">
+      <div class="widget feature-panel image-widget">
     		<a href="#" title="#">
-    			<img title="Placeholder" alt="Placeholder" src="http://placekitten.com/g/350/225">
+    			<img title="Placeholder" alt="Placeholder" src="<?php echo get_template_directory_uri(); ?>/img/signup.jpg">
     		</a>
           </div>
-          <div class="widget feature-panel">
+          <div class="widget feature-panel image-widget">
     		<a href="#" title="#">
-    			<img title="Placeholder" alt="Placeholder" src="http://placekitten.com/g/350/95">
+    			<img title="Placeholder" alt="Placeholder" src="<?php echo get_template_directory_uri(); ?>/img/newsletter.jpg">
     		</a>
       </div>
     </div>
@@ -54,19 +54,19 @@
         <i class="graphic-capitol"></i>
         <h4 class="main-font">Pioneers in Political Intelligence</h4>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-         <a class="uppercase" href="#" title="Learn More">Learn More ></a>
+         <a class="uppercase purple strong" href="#" title="Learn More">Learn More ></a>
      </div>
       <div class="widget graphic-widget">
         <i class="graphic-bullhorn"></i>
         <h4 class="main-font">Influence Public-Policy</h4>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        <a class="uppercase" href="#" title="Learn More">Learn More ></a>
+        <a class="uppercase purple strong" href="#" title="Learn More">Learn More ></a>
       </div>
       <div class="widget graphic-widget">
         <i class="graphic-nodes"></i>
         <h4 class="main-font">Build Coalitions</h4>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        <a class="uppercase" href="#" title="Learn More">Learn More ></a>
+        <a class="uppercase purple strong" href="#" title="Learn More">Learn More ></a>
       </div>
     </div>
     <div class="widget-area full group" id="widget-area-4">
--- wp-content/themes/first_street/header.php
+++ wp-content/themes/first_street/header.php
@@ -55,7 +55,7 @@
 
     </ul>
 
-    <ul id="primary-nav" class="nav pull-right clear-right separated-nav dark-gray">
+    <ul id="primary-nav" class="nav pull-right clear-right separated-nav dark-gray font-size-18">
       <li>Take a Tour</li>
       <li>Get Started</li>
       <li>What's New</li>
