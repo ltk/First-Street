031f5d49b571d563d4ebac01c2faec510bea5a2a
Lawson Kurtz <lawson.kurtz@gmail.com>
2012-06-27 12:38:11 -0400

    added table prefix to the environment

--- wp-config.php
+++ wp-config.php
@@ -62,7 +62,7 @@ define('DB_COLLATE', '');
  * You can have multiple installations in one database if you give each a unique
  * prefix. Only numbers, letters, and underscores please!
  */
-$table_prefix  = 'wp_';
+$table_prefix  = $_ENV["TABLE_PREFIX"];
 
 /**
  * WordPress Localized Language, defaults to English.
